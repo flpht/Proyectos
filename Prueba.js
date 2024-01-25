cc3ba6065fe8870eb40c753c3c0f44630ced38a8
noelSanabria <no.sanabria@duocuc.cl>
2024-01-25 16:43:40 -0300

    pruebas

--- Prueba.js
+++ Prueba.js
@@ -1,4 +1,9 @@
-Nombre = "Noel";
-frase = "soy " + Nombre  + " y estoy Estudiando";
+let titulo =  document.querySelector('h1');
+titulo.innerHTML = 'Bienvenidos';
 
-document.write(frase)
+let parrafo = document.querySelector('p');
+parrafo.innerHTML = 'Ya es Hora del desafio ';
+
+  function intentoDeUsuario () {
+alert('CLick desde el boton')
+  }
