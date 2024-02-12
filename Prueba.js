7aadf9f8342b3427f284fe31cf97c80bb7775e09
noelSanabria <no.sanabria@duocuc.cl>
2024-02-12 09:37:53 -0300

    upp

--- Prueba.js
+++
@@ -1,9 +0,0 @@
-let titulo =  document.querySelector('h1');
-titulo.innerHTML = 'Bienvenidos';
-
-let parrafo = document.querySelector('p');
-parrafo.innerHTML = 'Ya es Hora del desafio ';
-
-  function intentoDeUsuario () {
-alert('CLick desde el boton')
-  }
