c41c8a0e90bdc0a080519adceb15cd61544536c5
noelSanabria <no.sanabria@duocuc.cl>
2024-05-13 16:31:17 -0400

    Trabajo terminado solo falta diseño(opcional)

--- api.py
+++ api.py
@@ -1,11 +1,16 @@
-
-
+from webpay_plus_config import transaction
+from transbank.webpay.webpay_plus.transaction import Transaction
+from transbank.common.options import WebpayOptions
+from transbank.common.integration_type import IntegrationType
+from transbank.common.integration_commerce_codes import IntegrationCommerceCodes
+from transbank.common.integration_api_keys import IntegrationApiKeys
 from flask import Flask, render_template, request, jsonify
 import mysql.connector
 import uuid
 import my_bcchapi
 
 app = Flask(__name__)
+
 
 # Configuración de la base de datos
 db = mysql.connector.connect(
@@ -18,14 +23,19 @@ db = mysql.connector.connect(
 
 # Ruta para la página de inicio
 @app.route('/')
-def index():
+def home():
+    return render_template('index.html')
+
+@app.route('/inicio')
+def productos():
+    return render_template('inicio.html')
+
+
+@app.route('/conversor')
+def conversor():
     tasa_usd_clp = my_bcchapi.obtener_tasa_de_cambio()
     return render_template('conversor_moneda.html', tasa_usd_clp=tasa_usd_clp)
 
-# Ruta para la página de inicio 
-@app.route('/inicio')
-def inicio():
-    return render_template('inicio.html')
 
 # Ruta para agregar un producto
 @app.route('/productos', methods=['POST'])
@@ -109,6 +119,86 @@ def convertir():
             return jsonify({'error': 'No se pudo obtener la tasa de cambio'}), 500
     except Exception as e:
         return jsonify({'error': str(e)}), 500
+    
+@app.route('/iniciar_pago', methods=['POST'])
+def iniciar_pago():
+
+    try:
+        buy_order = request.form['buy_order']
+        session_id = request.form['session_id']
+        amount = float(request.form['amount'])
+        return_url = request.form['return_url']
+
+        response = transaction.create(buy_order, session_id, amount, return_url)
+        token = response['token']
+        payment_url = response['url']
+
+        return render_template('payment_form.html', payment_url=payment_url, token=token)
+    
+    except Exception as e:
+        print("Error al crear la transacción:", str(e))
+        error_message = 'Ocurrió un error al crear la transacción. Por favor, intenta nuevamente.'
+        return render_template('error.html', error_message=error_message)
+    
+
+@app.route('/confirmacion', methods=['GET', 'POST'])
+def confirmacion():
+    print("Llegada a la función confirmacion()")
+    print("Método de la solicitud:", request.method)
+
+    if request.method == 'POST':
+        token = request.form.get('token_ws')
+    else:
+        token = request.args.get('token_ws')
+
+    print("Token recibido:", token)
+
+    if token:
+        try:
+            # Configurar las opciones de Webpay
+            webpay_options = WebpayOptions(IntegrationCommerceCodes.WEBPAY_PLUS, IntegrationApiKeys.WEBPAY, IntegrationType.TEST)
+
+            # Crear una instancia de la transacción con las opciones de Webpay
+            transaction = Transaction(webpay_options)
+
+            print("Confirmando la transacción...")
+
+            # Confirmar la transacción
+            response = transaction.commit(token)
+
+            print("Respuesta de la confirmación:", response)
+
+            if response['response_code'] == 0:
+                # La transacción fue confirmada exitosamente
+                print("La transacción fue confirmada exitosamente")
+                return render_template('confirmacion.html', response=response)
+            else:
+                # La transacción no fue confirmada
+                print("La transacción no fue confirmada")
+                error_message = 'La transacción no pudo ser confirmada. Por favor, intenta nuevamente.'
+                return render_template('error.html', error_message=error_message)
+        except Exception as e:
+            print("Error al confirmar la transacción:")
+            print(str(e))
+            import traceback
+            traceback.print_exc()
+            error_message = 'Ocurrió un error al procesar la transacción. Por favor, intenta nuevamente.'
+            return render_template('error.html', error_message=error_message)
+    else:
+        # El token no fue recibido
+        print("Token no proporcionado o inválido")
+        error_message = 'Token no proporcionado o inválido.'
+        return render_template('error.html', error_message=error_message)
+
+
+@app.route('/retorno', methods=['GET', 'POST'])
+def retorno():
+    if request.method == 'POST':
+        token_ws = request.form.get('token_ws')
+    else:
+        token_ws = request.args.get('token_ws')
+
+    return confirmacion(token_ws)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0')
